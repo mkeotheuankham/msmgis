40041d476c0df41d00c92694dd56cb1b8709e1f3
Mitsamay Keotheuankham <mkeotheuankham@gmail.com>
2025-08-05 18:39:54 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This allows access from the Ngrok URL
+    allowedHosts: [
+      "deciding-winning-yak.ngrok-free.app",
+      // You can add other hosts here if needed
+      // 'another-host.com',
+    ],
+  },
 });
